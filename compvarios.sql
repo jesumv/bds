40a0a73243e7f8eed61254fc87bf605c5154e600
Jesus Maldonado <jmaldonadoca@gmail.com>
2017-09-28 14:36:13 -0500

    cambios a tabla parametros
    cambios a la tabla de paramatros

--- compvarios.sql
+++ compvarios.sql
@@ -1,3 +1,10 @@
 SELECT * FROM tblregistro;
 SELECT * FROM tblcat;
+SELECT * FROM tbllugar;
+SELECT * FROM tblclase;
 
+-- lista de competencia
+
+SELECT t1.fecha,t1.producto, CONCAT(t2.nombre," ", t2.marca, " ",t2.cant," ",t3.nombre),t1.precio, t1.punit, t4.nombre FROM tblregistro AS t1 
+INNER JOIN tblproducto AS t2 ON t1.producto = t2.idproducto INNER JOIN tblunids AS t3 ON t2.unid = t3.idunids 
+INNER JOIN tbllugar AS t4 ON t1.lugar = t4.idlugar ORDER BY t2.nombre;
--- vtas0a1.sql
+++ vtas0a1.sql
@@ -1,13 +1,13 @@
 -- ingresar operaciones de diario en 0 a 1
 -- revisar referencias
-SELECT * FROM diario WHERE (cuenta LIKE ('401%') AND facturar = 0 AND fecha >'2017-05-31' AND fecha <'2017-07-01') ORDER BY iddiario;
+SELECT * FROM diario WHERE (cuenta LIKE ('401%') AND facturar = 0 AND fecha >'2017-06-30' AND fecha <>'2017-07-19') ORDER BY iddiario;
 -- corregir fecha por referencia 
-UPDATE diario SET fecha = '2017-07-19',facturar = 1 , coment = 'correc fecha' WHERE referencia = 'pd105';
+UPDATE diario SET facturar = 1  WHERE referencia = 'pd126';
 
-UPDATE inventario SET fechamov = '2017-07-19', factu = 1 ,comentarios = 'ajust' WHERE idoc = '105' AND haber <>0;
+UPDATE inventario SET  factu = 1  WHERE idoc = '126' AND haber <>0;
  
-UPDATE pedidos SET fecha = '2017-07-19', fechapago = '2017-07-19', facturar = 1 ,coment = 'ajust' WHERE idpedidos = 105;
+UPDATE pedidos SET  facturar = 1  WHERE idpedidos = 126;
 
-SELECT * FROM diario WHERE referencia = 'pd104';
+SELECT * FROM diario WHERE referencia = 'pd126';
 
-SELECT SUM(haber) FROM diario WHERE cuenta='401.04' AND fecha >'2017-06-30';
+SELECT SUM(haber) FROM diario WHERE cuenta='401.04' AND fecha ='2017-07-19';
